e7507f3e5159faaa9d5e8e412b3b65282354c950
jeh-p-em <50097354+jeh-p-em@users.noreply.github.com>
2026-08-11 00:21:44 -0500

    Update compress_mkv.sh

--- compress_mkv.sh
+++ compress_mkv.sh
@@ -10,60 +10,59 @@ rm -rf /opt/compress_mkv/attachments*
 find . -type f \( -iname "*.mkv" -o -iname "*.mp4" \) -print0 | sort -zV |
 
 while IFS= read -r -d '' file; do
-  compressed=$(ffprobe -v error \
-    -show_entries format_tags=comment \
-    -of default=noprint_wrappers=1:nokey=1 \
-    "$file")
+	compressed=$(ffprobe -v error \
+		-show_entries format_tags=comment \
+		-of default=noprint_wrappers=1:nokey=1 \
+		"$file")
 
-  if [[ "$compressed" == "COMPRESSED_V$version" ]]; then
-    echo "File already compressed: $file"
-    continue
-  fi
+	if [[ "$compressed" == "COMPRESSED_V$version" ]]; then
+		echo "File already compressed: $file"
+		continue
+	fi
 
-  SECONDS=0
-  echo "Processing: $file"
-  basename_file=$(basename "$file")
-  ext="${file##*.}"
+	SECONDS=0
+	echo "Processing: $file"
+	basename_file=$(basename "$file")
+	ext="${file##*.}"
 
-  tmp_file=$(mktemp --suffix=".$ext" /opt/compress_mkv/ffmpeg.XXXXXX)
-  output_file=$(mktemp --suffix=".$ext" /opt/compress_mkv/output.XXXXXX)
-  attach_dir=$(mktemp -d /opt/compress_mkv/attachments.XXXXXX)
+	tmp_file=$(mktemp --suffix=".$ext" /opt/compress_mkv/ffmpeg.XXXXXX)
+	output_file=$(mktemp --suffix=".$ext" /opt/compress_mkv/output.XXXXXX)
+	attach_dir=$(mktemp -d /opt/compress_mkv/attachments.XXXXXX)
 
-  echo "Copying: $file to $tmp_file"
-  if ! cp -- "$file" "$tmp_file"; then
-    echo "Failed to copy $file"
-    rm -f "$tmp_file"
-    rm -rf "$attach_dir"
-    continue
-  fi
+	echo "Copying: $file to $tmp_file"
+	if ! cp -- "$file" "$tmp_file"; then
+		echo "Failed to copy $file"
+		rm -f "$tmp_file"
+		rm -rf "$attach_dir"
+		continue
+	fi
 
-  before_size=$(( $(stat -c %s "$tmp_file") / 1024 / 1024 ))
+	before_size=$(( $(stat -c %s "$tmp_file") / 1024 / 1024 ))
+	echo "Checking attachments..."
+	attachment_count=$(mkvmerge -J "$tmp_file" | jq '.attachments | length')
 
-  echo "Checking attachments..."
-  attachment_count=$(mkvmerge -J "$tmp_file" | jq '.attachments | length')
+	if [[ "$attachment_count" -gt 0 ]]; then
+		echo "Extracting $attachment_count attachments..."
+		while IFS=: read -r id name; do
+		mkvextract -q attachments "$tmp_file" \
+			"$id:$attach_dir/$name"
+		done < <(
+			mkvmerge -J "$tmp_file" |
+			jq -r '.attachments[] | "\(.id):\(.file_name)"'
+		)
+	else
+		echo "No attachments found."
+	fi
 
-  if [[ "$attachment_count" -gt 0 ]]; then
-    echo "Extracting $attachment_count attachments..."
-    while IFS=: read -r id name; do
-      mkvextract -q attachments "$tmp_file" \
-        "$id:$attach_dir/$name"
-    done < <(
-      mkvmerge -J "$tmp_file" |
-      jq -r '.attachments[] | "\(.id):\(.file_name)"'
-    )
-  else
-    echo "No attachments found."
-  fi
-
-  echo "Encoding: $tmp_file to $output_file"
-  if ffmpeg \
-    -hide_banner \
+	echo "Encoding: $tmp_file to $output_file"
+	if ffmpeg \
+		-hide_banner \
 		-v error \
 		-nostdin \
 		-y \
 		-probesize 100M \
 		-analyzeduration 100M \
-    -vaapi_device /dev/dri/renderD128 \
+		-vaapi_device /dev/dri/renderD128 \
 		-i "$tmp_file" \
 		-filter_complex "[0:v:0]format=p010le,hwupload[v]" \
 		-x265-params no-open-gop=1:keyint=600:gop-lookahead=11:bframes=7:weightb=1:hme=1:strong-intra-smoothing=0:rect=0:aq-mode=4 \
@@ -74,8 +73,8 @@ while IFS= read -r -d '' file; do
 		-map_metadata 0 \
 		-map_chapters 0 \
 		-metadata comment="COMPRESSED_V$version" \
-    -c:v:0 hevc_vaapi \
-    -qp:v:0 26 \
+		-c:v:0 hevc_vaapi \
+		-qp:v:0 26 \
 		-maxrate 3M \
 		-bufsize 8M \
 		-c:v:1 copy \
@@ -85,7 +84,7 @@ while IFS= read -r -d '' file; do
 		-c:s copy \
 		"$output_file"
 
-  then
+	then
 		if [[ "$attachment_count" -gt 0 ]]; then
 			echo "Restoring attachments..."
 			for attachment in "$attach_dir"/*; do
@@ -100,47 +99,47 @@ while IFS= read -r -d '' file; do
 			done
 		fi
 
-  after_size=$(( $(stat -c %s "$output_file") / 1024 / 1024 ))
+		after_size=$(( $(stat -c %s "$output_file") / 1024 / 1024 ))
 
-  if [[ "$after_size" -gt "$before_size" ]]; then
-    before_size="${before_size}MB"
-    after_size="${after_size}MB"
-    echo "Did not shrink file size: $file - $before_size > $after_size"
-    curl -s -o /dev/null -d "$basename_file
+		if [[ "$after_size" -gt "$before_size" ]]; then
+			before_size="${before_size}MB"
+			after_size="${after_size}MB"
+			echo "Did not shrink file size: $file - $before_size > $after_size"
+			curl -s -o /dev/null -d "$basename_file
 $before_size > $after_size" "ntfy.sh/$ntfy"
-    rm -f -- "$output_file"
-    rm -f -- "$tmp_file"
-    rm -rf -- "$attach_dir"
-    echo "----------------------------------------"
-    continue
-	fi
+			rm -f -- "$output_file"
+			rm -f -- "$tmp_file"
+			rm -rf -- "$attach_dir"
+			echo "----------------------------------------"
+			continue
+		fi
 
-  before_size="${before_size}MB"
-  after_size="${after_size}MB"
+		before_size="${before_size}MB"
+		after_size="${after_size}MB"
 
-  echo "Moving: $output_file to $file"
-  mv -f -- "$output_file" "$file"
+		echo "Moving: $output_file to $file"
+		mv -f -- "$output_file" "$file"
 
-  elapsed=$(printf "%02d:%02d:%02d" \
-    $((SECONDS/3600)) \
-    $(((SECONDS%3600)/60)) \
-    $((SECONDS%60)))
+		elapsed=$(printf "%02d:%02d:%02d" \
+			$((SECONDS/3600)) \
+			$(((SECONDS%3600)/60)) \
+			$((SECONDS%60)))
 
-  echo "Successfully compressed: $file - $before_size > $after_size"
-  curl -s -o /dev/null -d "$basename_file
+		echo "Successfully compressed: $file - $before_size > $after_size"
+		curl -s -o /dev/null -d "$basename_file
 $before_size > $after_size 
 $elapsed" "ntfy.sh/$ntfy"
 
-else
-    echo "Error compressing: $tmp_file. Original kept."
-    curl -s -o /dev/null -d "$basename_file" "ntfy.sh/$ntfy"
-  fi
+	else
+		echo "Error compressing: $tmp_file. Original kept."
+		curl -s -o /dev/null -d "$basename_file" "ntfy.sh/$ntfy"
+	fi
 
-  echo "Cleaning up"
-  rm -f -- "$output_file"
-  rm -f -- "$tmp_file"
-  rm -rf -- "$attach_dir"
-  echo "----------------------------------------"
+	echo "Cleaning up"
+	rm -f -- "$output_file"
+	rm -f -- "$tmp_file"
+	rm -rf -- "$attach_dir"
+	echo "----------------------------------------"
 done
 
 sleep 10
